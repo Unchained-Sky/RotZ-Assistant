ba0c37c15948cce65e97c2d71d7226b231c738bb
JoelDohmKaratjas <33188727+JoelDohmKaratjas@users.noreply.github.com>
2025-04-26 00:29:50 +0100

    ✨ Multiple character sheets

--- src/sections/CharacterSheet.tsx
+++ src/sections/CharacterSheet.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
-import { ActionIcon, Button, Card, Divider, FileInput, Group, List, Modal, Stack, Textarea, Title, Tooltip } from '@mantine/core'
+import { ActionIcon, Button, Card, Divider, FileInput, Group, List, Modal, Stack, Tabs, Text, Textarea, Title, Tooltip } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
-import { IconFileImport, IconReload } from '@tabler/icons-react'
+import { IconFileDots, IconFileImport } from '@tabler/icons-react'
 import { useState } from 'react'
 import Markdown from 'react-markdown'
+import { Temporal } from 'temporal-polyfill'
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 import { tooltipProps } from '../utils/tooltipProps'
@@ -15,100 +16,161 @@ const PLACEHOLDER_TEXT = `# Character Sheet
 - Power: 80`
 
 export default function CharacterSheet() {
-	const [importModal, importModalHandlers] = useDisclosure(false)
-	const characterSheet = useCharacterSheetStore(state => state.characterSheet)
-
 	return (
-		<>
-			<Card component={Stack}>
-				<Group justify='space-between'>
-					<Title order={2}>Character Sheet</Title>
-					<Group gap='xs'>
-						<Tooltip label='Import Character Sheet' {...tooltipProps}>
-							<ActionIcon variant='default' onClick={importModalHandlers.open}>
-								<IconFileImport />
-							</ActionIcon>
-						</Tooltip>
-						<Tooltip label='Reset Character Sheet' {...tooltipProps}>
-							<ActionIcon variant='default' onClick={() => useCharacterSheetStore.getState().setCharacterSheet('')}>
-								<IconReload />
-							</ActionIcon>
-						</Tooltip>
-					</Group>
+		<Card component={Stack}>
+			<Group justify='space-between'>
+				<Title order={2}>Character Sheet</Title>
+				<Group gap='xs'>
+					<ImportSheetModal />
+					<RemoveSheetModal />
 				</Group>
-				<MantineMarkdown markdown={characterSheet} />
-			</Card>
-
-			<ImportSheetModal opened={importModal} close={importModalHandlers.close} />
-		</>
+			</Group>
+			<CharacterTabs />
+		</Card>
 	)
 }
 
-type ImportSheetModalProps = {
-	opened: boolean
-	close: () => void
-}
+function ImportSheetModal() {
+	const [opened, { open, close }] = useDisclosure(false)
 
-function ImportSheetModal({ opened, close }: ImportSheetModalProps) {
 	const [file, setFile] = useState<File | null>(null)
 	const [text, setText] = useState<string>('')
 
 	return (
-		<Modal
-			opened={opened}
-			onClose={close}
-			title='Import Character Sheet'
-			onExitTransitionEnd={() => {
-				setFile(null)
-				setText('')
-			}}
-		>
-			<Stack>
-				<FileInput
-					label='Markdown File'
-					clearable
-					placeholder='character-sheet.md'
-					accept='.md'
-					value={file}
-					onChange={setFile}
-					disabled={text.length > 0}
-				/>
-				<Divider label='or' />
-				<Textarea
-					label='Text'
-					autosize
-					minRows={2}
-					maxRows={5}
-					placeholder={PLACEHOLDER_TEXT}
-					value={text}
-					onChange={event => setText(event.currentTarget.value)}
-					disabled={file !== null}
-				/>
-				<Group>
-					<Button variant='default' onClick={close}>Cancel</Button>
-					<Button
-						type='submit'
-						flex={1}
-						onClick={() => {
-							const setSheet = useCharacterSheetStore.getState().setCharacterSheet
-							if (file) {
-								const reader = new FileReader()
-								reader.onload = () => {
-									setSheet(reader.result as string)
+		<>
+			<Tooltip label='Import Character Sheet' {...tooltipProps}>
+				<ActionIcon variant='default' onClick={open}>
+					<IconFileImport />
+				</ActionIcon>
+			</Tooltip>
+
+			<Modal
+				opened={opened}
+				onClose={close}
+				title='Import Character Sheet'
+				onExitTransitionEnd={() => {
+					setFile(null)
+					setText('')
+				}}
+			>
+				<Stack>
+					<FileInput
+						label='Markdown File'
+						clearable
+						placeholder='character-sheet.md'
+						accept='.md'
+						value={file}
+						onChange={setFile}
+						disabled={text.length > 0}
+					/>
+					<Divider label='or' />
+					<Textarea
+						label='Text'
+						autosize
+						minRows={2}
+						maxRows={5}
+						placeholder={PLACEHOLDER_TEXT}
+						value={text}
+						onChange={event => setText(event.currentTarget.value)}
+						disabled={file !== null}
+					/>
+					<Group>
+						<Button variant='default' onClick={close}>Cancel</Button>
+						<Button
+							type='submit'
+							flex={1}
+							onClick={() => {
+								const setSheet = useCharacterSheetStore.getState().addCharacterSheet
+								if (file) {
+									const reader = new FileReader()
+									reader.onload = () => {
+										setSheet(reader.result as string)
+										close()
+									}
+									reader.readAsText(file)
+								} else {
+									setSheet(text)
 									close()
 								}
-								reader.readAsText(file)
-							} else {
-								setSheet(text)
-								close()
-							}
-						}}
-					>Confirm
-					</Button>
-				</Group>
-			</Stack>
-		</Modal>
+							}}
+						>Confirm
+						</Button>
+					</Group>
+				</Stack>
+			</Modal>
+		</>
 	)
+}
+
+function RemoveSheetModal() {
+	const characterSheet = useCharacterSheetStore(state => state.characterSheet)
+	const [opened, { open, close }] = useDisclosure(false)
+
+	return (
+		<>
+			<Tooltip label='Remove Character Sheet' {...tooltipProps}>
+				<ActionIcon variant='default' onClick={open}>
+					<IconFileDots />
+				</ActionIcon>
+			</Tooltip>
+
+			<Modal
+				opened={opened}
+				onClose={close}
+				title='Remove Character Sheet'
+			>
+				<Stack>
+					{
+						Object.keys(characterSheet).map((playerName, i) => {
+							const [name, added] = playerName.split('::')
+							const addedTemporal = Temporal.Instant.fromEpochMilliseconds(+added)
+							const now = Temporal.Now.instant()
+							const duration = now.since(addedTemporal, { smallestUnit: 'second', largestUnit: 'hours' })
+							return (
+								<Group key={i} justify='space-between'>
+									<Stack gap={0}>
+										<Text>{name}</Text>
+										<Text size='xs'>Added: {duration.toLocaleString('en-GB')} ago</Text>
+									</Stack>
+									<Button color='red' onClick={() => useCharacterSheetStore.getState().removeCharacterSheet(playerName)}>Remove</Button>
+								</Group>
+							)
+						})
+					}
+				</Stack>
+			</Modal>
+		</>
+	)
+}
+
+function CharacterTabs() {
+	const characterSheet = useCharacterSheetStore(state => state.characterSheet)
+	const activeCharacter = useCharacterSheetStore(state => state.activeCharacter)
+
+	return Object.keys(characterSheet).length
+		? (
+			<Tabs value={activeCharacter} onChange={name => useCharacterSheetStore.setState({ activeCharacter: name ?? '' })}>
+				<Tabs.List>
+					{
+						Object.keys(characterSheet).reverse().map((playerName, i) => {
+							return (
+								<Tabs.Tab key={i} value={playerName}>{playerName.split('::')[0]}</Tabs.Tab>
+							)
+						})
+					}
+				</Tabs.List>
+				{
+					Object.keys(characterSheet).map((playerName, i) => {
+						return (
+							<Tabs.Panel key={i} value={playerName}>
+								<MantineMarkdown markdown={characterSheet[playerName]} />
+							</Tabs.Panel>
+						)
+					})
+				}
+			</Tabs>
+		)
+		: null
 }
 
 type MantineMarkdownProps = {
@@ -151,13 +213,31 @@ function MantineMarkdown({ markdown }: MantineMarkdownProps) {
 }
 
 type CharacterSheetStore = {
-	characterSheet: string
-	setCharacterSheet: (sheet: string) => void
+	characterSheet: Record<string, string>
+	activeCharacter: string
+	addCharacterSheet: (sheet: string) => void
+	removeCharacterSheet: (name: string) => void
 }
 
-const useCharacterSheetStore = create<CharacterSheetStore>()(persist((set, _get) => ({
-	characterSheet: '',
-	setCharacterSheet: sheet => {
-		set({ characterSheet: sheet })
+const useCharacterSheetStore = create<CharacterSheetStore>()(persist((set, get) => ({
+	characterSheet: {},
+	activeCharacter: '',
+	addCharacterSheet: sheet => {
+		const firstLine = sheet.split('\n')[0]
+		const name = firstLine.startsWith('# ') ? firstLine.replace('# ', '') : 'Unknown'
+		const title = `${name}::${+new Date()}`
+		set(state => ({
+			characterSheet: {
+				...state.characterSheet,
+				[title]: sheet
+			},
+			activeCharacter: title
+		}))
+	},
+	removeCharacterSheet: name => {
+		const { [name]: _name, ...characterSheet } = get().characterSheet
+		const { activeCharacter } = get()
+		if (activeCharacter === name) set({ activeCharacter: Object.keys(characterSheet).at(-1) ?? '' })
+		set({ characterSheet })
 	}
 }), { name: 'rotz-assistant-character-sheet-store' }))
